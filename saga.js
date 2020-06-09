93d01edb8e75f072f1ebbb07eff1bec9b77df51b
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-06-09 14:03:37 -0400

    add sagas for incrementAsync

--- saga.js
+++ saga.js
@@ -1,3 +1,16 @@
+import { put, takeEvery } from "redux-saga"
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
 export function* helloSaga() {
   console.log("Hello Sagas!")
+}
+
+export function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: "INCREMENT" })
+}
+
+export function* watchIncrementAsync() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync)
 }
